897686c015ac8187d56b081d4badec11cf271ff1
Amirhosein Malekpour <work.malekpour99@gmail.com>
2026-03-17 11:37:20 +0330

    Solved Norouz feast problem (28947)

---
+++ norouz_feast.py
@@ -0,0 +1,9 @@
+# https://quera.org/problemset/28947
+# ----------------------------------
+
+seven_s = ("sib", "samanoo", "serkeh", "sekeh", "sabzeh", "sonbol", "somagh")
+
+n = int(input().strip())
+
+for i in range(n):
+    print(seven_s[i])
