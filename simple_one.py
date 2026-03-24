80edc2aa42667e9aee69712a741b07510de1f194
Amirhosein Malekpour <work.malekpour99@gmail.com>
2026-03-24 09:41:59 +0330

    Solved a simple question problem (2885)

---
+++ simple_one.py
@@ -0,0 +1,11 @@
+# https://quera.org/problemset/2885
+# ---------------------------------
+
+from typing import Final
+
+MESSAGE: Final = "man khoshghlab hastam"
+
+n = int(input().strip())
+
+for _ in range(n):
+    print(MESSAGE)
