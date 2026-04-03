bed4fb66530d84b1a4b94746bb8d91b925b7dc77
Amirhosein Malekpour <work.malekpour99@gmail.com>
2026-04-03 20:56:14 +0330

    Solved hello world-cup problem (126106)

---
+++ Go/hello_worldcup/main.go
@@ -0,0 +1,9 @@
+// https://quera.org/problemset/126106
+// -----------------------------------
+package main
+
+import "fmt"
+
+func HelloCodeCup(n int) string {
+	return fmt.Sprintf("Hello CodeCup %d", n)
+}
