904f18cc38928fa941c8820d7c527fa37faf6b61
Amirhosein Malekpour <work.malekpour99@gmail.com>
2026-04-10 22:14:35 +0330

    Solved registrations problem (275457)

---
+++ registrations.py
@@ -0,0 +1,9 @@
+# https://quera.org/problemset/275457
+# -----------------------------------
+
+registration_count = int(input().strip())
+
+if registration_count > 50:
+    print("Yes")
+else:
+    print("No")
